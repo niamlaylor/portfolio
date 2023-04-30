aa937326c53018fd5fcb20cc0979c8951ae84483
Liam Naylor <liam@liamnaylor.com>
2023-04-30 13:41:41 -0700

    Add weather card component

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import WeatherCard from "../../components/WeatherCard";
 
 export default function Home() {
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
+      <WeatherCard location={'Vancouver'} temperature={87} description={'Cloudy'}/>
     </main>
   )
 }
