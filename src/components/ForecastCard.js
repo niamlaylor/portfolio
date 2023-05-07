d114ec86692764a6381acd66e95db31869429b56
Liam Naylor <liam@liamnaylor.com>
2023-05-07 11:23:38 -0700

    Add map for each forecast day

--- src/components/ForecastCard.js
+++ src/components/ForecastCard.js
@@ -3,12 +3,20 @@ import ForecastCardLine from "./ForecastCardLine";
 export default function ForecastCard ({ forecastData, unit }) {
 
   const forecastArray = forecastData.forecast.forecastday;
+  const forecastCardLines = forecastArray.map((day, index) => {
+    return (
+      <ForecastCardLine
+        key={index}
+        forecastDay={day}
+      />
+    )
+  })
 
   return (
     <div className="rounded-lg p-4 w-80 mt-10 border border-gray-300">
       <table className="w-full">
         <tbody>
-          <ForecastCardLine forecastDay={forecastArray[0]} />
+          {forecastCardLines}
           <tr>
             <td className="font-semibold">Region:</td>
             <td>70</td>
--- src/components/ForecastCardLine.js
+++ src/components/ForecastCardLine.js
@@ -1,7 +1,12 @@
+import { useEffect } from "react";
+
 export default function ForecastCardLine ({ forecastDay }) {
+  useEffect(() => {
+    console.log('HERE IS HE FORECAST DAY:', forecastDay)
+  },[])
   return (
     <tr>
-      <td className="font-semibold">Day:</td>
+      <td className="font-semibold">Date:</td>
       <td>{forecastDay.date}</td>
     </tr>
   )
