d019a47dde223e542ed7ac0bb0475815664f3bf3
Liam Naylor <liam@liamnaylor.com>
2023-05-12 12:26:41 -0700

    Add margin for contact icons

--- src/components/nav-items/NavContactLink.js
+++ src/components/nav-items/NavContactLink.js
@@ -3,49 +3,51 @@ export default function NavContactLink( { children } ) {
   return (
     <>
       { children === 'Github' && <>
-        <img
-          src={'https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/github-mark.png'}
-          width={20}
-          height={20}
-        />
         <a 
           href="https://github.com/niamlaylor" 
           target="_blank" 
-          className={linkClass}
+          className={`${linkClass} flex`}
         >
+          <img
+          src={'https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/github-mark.png'}
+          width={20}
+          height={20}
+          className={"mr-1"}
+        />
           {children}
         </a>
       </>
       }
       { children === 'LinkedIn' && 
       <>
-        <img
-          src={'https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/linkedin-web.png'}
-          width={20}
-          height={20}
-        />
         <a 
           href="https://www.linkedin.com/in/liamnnaylor/" 
           target="_blank" 
-          className={linkClass}
+          className={`${linkClass} flex`}
         >
+          <img
+          src={'https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/linkedin-web.png'}
+          width={20}
+          height={20}
+          className={"mr-1"}
+        />
           {children}
         </a>
       </>
       }
       { children === 'Medium' && 
       <>
-        <img
-          src={'https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/medium-logo.png'}
-          width={20}
-          height={20}
-          className="rounded-full"
-        />
         <a 
           href="https://medium.com/@liamnaylorr" 
           target="_blank" 
-          className={linkClass}
+          className={`${linkClass} flex`}
         >
+          <img
+          src={'https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/medium-logo.png'}
+          width={20}
+          height={20}
+          className="rounded-full mr-1"
+        />
           {children}
         </a>
       </>
