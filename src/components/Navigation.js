4b7f00205fda55e12073a186712a40d277549869
Liam Naylor <liam@liamnaylor.com>
2023-05-12 10:11:56 -0700

    Add responsive styles for nav + contact row of a elements

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -2,11 +2,16 @@ export default function Navigation() {
   return (
     <nav className="bg-custom-white py-10 px-8">
       <div className="container mx-auto flex items-center justify-center">
-        <a href="/" className="text-black tracking-nav-wide text-h1 font-semibold">Liam Naylor</a>
+        <a href="/" className="text-black tracking-nav-wide lg:text-h1 md:text-h1-tablet sm:text-h1-mobile font-semibold">Liam Naylor</a>
       </div>
       <div className="container mx-auto flex items-center justify-center">
         <a href="/copywriting" className="text-black text-lg font-semibold">Copywriter</a>
         <a href="/development" className="text-black text-lg font-semibold">Developer</a>
+      </div>
+      <div className="container mx-auto mt-3 flex items-center justify-center">
+        <a href="https://github.com/niamlaylor" target="_blank" className="text-black text-sm font-semibold">Github</a>
+        <a href="https://www.linkedin.com/in/liamnnaylor/" target="_blank" className="text-black text-sm font-semibold">LinkedIn</a>
+        <a href="/development" className="text-black text-sm font-semibold">Email me</a>
       </div>
     </nav>
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,16 +6,31 @@ module.exports = {
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
+    screens: {
+      'sm': '375px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '640px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       backgroundColor: {
         'custom-white': '#fbfbfd',
       },
       fontSize: {
-        'h1': '3rem'
+        'h1': '10rem',
+        'h1-mobile': '3rem',
+        'h1-tablet': '6rem'
       },
-      letterSpacing: {
-        'nav-wide': '0.2em'
-      }
     },
   },
   plugins: [],
