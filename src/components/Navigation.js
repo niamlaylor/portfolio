bc4b267b982ce5d77805c1639e84b8de123a5ef4
Liam Naylor <liam@liamnaylor.com>
2023-05-12 14:11:43 -0700

    Swap copy and dev cards + buttons

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,8 +8,8 @@ export default function Navigation() {
         <a href="/" className="text-black tracking-nav-wide lg:text-h1 md:text-h1-tablet sm:text-h1-mobile font-semibold">Liam Naylor</a>
       </div>
       <div className="container mx-auto flex items-center justify-center">
-        <NavSkillLink>Copywriting</NavSkillLink>
         <NavSkillLink>Development</NavSkillLink>
+        <NavSkillLink>Copywriting</NavSkillLink>
       </div>
       <div className="container mx-auto mt-4 flex items-center justify-center">
         <NavContactLink>Github</NavContactLink>
--- src/components/home/HomeCard.js
+++ src/components/home/HomeCard.js
@@ -3,8 +3,8 @@ import HomeCardPanel from "./HomeCardPanel";
 export default function HomeCard() {
   return(
     <section className="flex sm:flex-col md:flex-row lg:flex-row">
-      <HomeCardPanel bgColor={"copywriting-green"} textColor={"custom-gold"}>Copywriting</HomeCardPanel>
       <HomeCardPanel bgColor={"custom-gray"}>Development</HomeCardPanel>
+      <HomeCardPanel bgColor={"copywriting-green"} textColor={"custom-gold"}>Copywriting</HomeCardPanel>
     </section>
   );
 };
