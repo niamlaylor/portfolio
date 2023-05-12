ed4ea5f60897101fe6f5ab8ce2ab1d5f739e67db
Liam Naylor <liam@liamnaylor.com>
2023-05-12 09:28:29 -0700

    Add customer nav styles + sub nav items

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,8 +1,12 @@
 export default function Navigation() {
   return (
-    <nav className="bg-teal-500 py-4 px-8">
+    <nav className="bg-custom-white py-10 px-8">
       <div className="container mx-auto flex items-center justify-center">
-        <a href="/" className="text-white text-lg font-semibold">Liam</a>
+        <a href="/" className="text-black text-h1 font-semibold">Liam Naylor</a>
+      </div>
+      <div className="container mx-auto flex items-center justify-center">
+        <a href="/copywriting" className="text-black text-lg font-semibold">Copywriter</a>
+        <a href="/development" className="text-black text-lg font-semibold">Developer</a>
       </div>
     </nav>
   )
