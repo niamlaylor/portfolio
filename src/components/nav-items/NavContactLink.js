a30839a5f42d724e7f051eeb7964d6beefd27c41
Liam Naylor <liam@liamnaylor.com>
2023-05-12 10:40:04 -0700

    Add github logo + refactor contactlink

--- src/components/nav-items/NavContactLink.js
+++ src/components/nav-items/NavContactLink.js
@@ -2,20 +2,24 @@ export default function NavContactLink( { children } ) {
   const linkClass = "text-black text-sm ml-1 mr-1 font-semibold";
   return (
     <>
-      { children === 'Github' && <a 
-        href="https://github.com/niamlaylor" 
-        target="_blank" 
-        className={linkClass}
-      >
-        {children}
-      </a>}
+      { children === 'Github' && <>
+        <a 
+          href="https://github.com/niamlaylor" 
+          target="_blank" 
+          className={linkClass}
+        >
+          {children}
+        </a>
+      </>
+      }
       { children === 'LinkedIn' && <a 
         href="https://www.linkedin.com/in/liamnnaylor/" 
         target="_blank" 
         className={linkClass}
       >
         {children}
-      </a>}
+      </a>
+      }
     </>
   )
 };
