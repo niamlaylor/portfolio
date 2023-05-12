22b317f183f78364e21f46043bab2ee112154a9e
Liam Naylor <liam@liamnaylor.com>
2023-05-12 14:41:59 -0700

    Add list for portfolio items

--- src/components/home/HomeCard.js
+++ src/components/home/HomeCard.js
@@ -3,8 +3,8 @@ import HomeCardPanel from "./HomeCardPanel";
 export default function HomeCard() {
   return(
     <section className="flex sm:flex-col md:flex-row lg:flex-row">
-      <HomeCardPanel bgColor={"custom-gray"}>Development</HomeCardPanel>
-      <HomeCardPanel bgColor={"copywriting-green"} textColor={"custom-gold"}>Copywriting</HomeCardPanel>
+      <HomeCardPanel bgColor={"custom-gray"} type={"dev"}>Development</HomeCardPanel>
+      <HomeCardPanel bgColor={"copywriting-green"} textColor={"custom-gold"} type={"copy"}>Copywriting</HomeCardPanel>
     </section>
   );
 };
--- src/components/home/HomeCardPanel.js
+++ src/components/home/HomeCardPanel.js
@@ -1,11 +1,16 @@
-export default function HomeCardPanel({ children, bgColor, textColor }) {
+export default function HomeCardPanel({ children, bgColor, textColor, type }) {
+  const listItems = ["A", "B"];
+
   return (
     <article className={`flex flex-col items-center justify-start w-full h-screen bg-${bgColor}`}>
       <h1 className={`text-${textColor} text-h2-desktop mt-10 font-semibold`}>{children}</h1>
-      <div className="flex-1 w-full"></div>
-      <div className="flex-1 w-full"></div>
-      <div className="flex-1 w-full"></div>
-      <div className="flex-1 w-full"></div>
+      <div className="flex-1 w-full">
+        <ul className="flex flex-col justify-between items-stretch items w-full">
+          {listItems.map((item, index) => (
+            <li key={index} className="flex-1 items-center border">{item}</li>
+          ))}
+        </ul>
+      </div>
     </article>
   );
 };
