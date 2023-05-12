715a94d6b6f06f48f03a6f3156e70a1d5389386d
Liam Naylor <liam@liamnaylor.com>
2023-05-12 14:22:31 -0700

    Add div elements to each home card for projects

--- src/components/home/HomeCardPanel.js
+++ src/components/home/HomeCardPanel.js
@@ -1,7 +1,11 @@
 export default function HomeCardPanel({ children, bgColor, textColor }) {
-  return(
+  return (
     <article className={`flex flex-col items-center justify-start w-full h-screen bg-${bgColor}`}>
       <h1 className={`text-${textColor} text-h2-desktop mt-10 font-semibold`}>{children}</h1>
+      <div className="flex-1 w-full"></div>
+      <div className="flex-1 w-full"></div>
+      <div className="flex-1 w-full"></div>
+      <div className="flex-1 w-full"></div>
     </article>
   );
 };
