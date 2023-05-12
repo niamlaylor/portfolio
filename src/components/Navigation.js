c820e15703b8a5bba3829b5562b57792a45324a8
Liam Naylor <liam@liamnaylor.com>
2023-05-12 10:29:05 -0700

    Add lowercase children handling for skill link

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -8,8 +8,8 @@ export default function Navigation() {
         <a href="/" className="text-black tracking-nav-wide lg:text-h1 md:text-h1-tablet sm:text-h1-mobile font-semibold">Liam Naylor</a>
       </div>
       <div className="container mx-auto flex items-center justify-center">
-        <NavSkillLink>copywriting</NavSkillLink>
-        <NavSkillLink>development</NavSkillLink>
+        <NavSkillLink>Copywriting</NavSkillLink>
+        <NavSkillLink>Development</NavSkillLink>
       </div>
       <div className="container mx-auto mt-3 flex items-center justify-center">
         <NavContactLink>Github</NavContactLink>
--- src/components/nav-items/NavSkillLink.js
+++ src/components/nav-items/NavSkillLink.js
@@ -1,3 +1,11 @@
 export default function NavSkillLink( { children } ) {
-  return <a href={`${children}`} className="text-black text-lg ml-1 mr-1 font-semibold">{children}</a>
+  const lowerCaseChild = children.toLowerCase();
+  return (
+    <a 
+      href={`${lowerCaseChild}`} 
+      className="text-black text-lg ml-1 mr-1 font-semibold"
+    >
+      {children}
+    </a>
+  )
 };
