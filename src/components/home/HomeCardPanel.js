79520f8c845a3a17ad8e8c81bc0fd682883b0339
Liam Naylor <liam@liamnaylor.com>
2023-05-14 16:49:56 -0700

    Add padding to li array + center text

--- src/components/home/HomeCardPanel.js
+++ src/components/home/HomeCardPanel.js
@@ -1,14 +1,22 @@
 export default function HomeCardPanel({ children, bgColor, textColor, type }) {
   const listItems = ["A", "B"];
+  const listArray = listItems.map((item, index) => {
+    return (
+      <li
+        key={index}
+        className={`flex items-center justify-center w-full p-10`}
+      >
+        BC Ferries app
+      </li>
+    )
+  })
 
   return (
     <article className={`flex flex-col items-center justify-start w-full bg-${bgColor}`}>
       <h1 className={`text-${textColor} sm:text-h2-mobile md:text-h2-tablet lg:text-h2-desktop mt-10 font-semibold`}>{children}</h1>
       <div className="flex-1 w-full">
         <ul className="flex flex-col items-center">
-          <li>BC Ferries website</li>
-          <li>BC Ferries app</li>
-          <li>BC Ferries app</li>
+          {listArray}
         </ul>
       </div>
     </article>
