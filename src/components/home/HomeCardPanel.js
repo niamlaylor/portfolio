d12d1d25ae22d248a15118176096f618cecc62b4
Liam Naylor <liam@liamnaylor.com>
2023-05-14 16:25:48 -0700

    Add responsive font sizes to cards

--- src/components/home/HomeCardPanel.js
+++ src/components/home/HomeCardPanel.js
@@ -3,7 +3,7 @@ export default function HomeCardPanel({ children, bgColor, textColor, type }) {
 
   return (
     <article className={`flex flex-col items-center justify-start w-full h-screen bg-${bgColor}`}>
-      <h1 className={`text-${textColor} text-h2-desktop mt-10 font-semibold`}>{children}</h1>
+      <h1 className={`text-${textColor} sm:text-h2-mobile md:text-h2-tablet lg:text-h2-desktop mt-10 font-semibold`}>{children}</h1>
       <div className="flex-1 w-full">
         <ul className="flex flex-col justify-between items-stretch items w-full">
           {listItems.map((item, index) => (
