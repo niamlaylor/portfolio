185cb0a509922d31baa74f65f8f3db1cfc991e8c
Liam Naylor <liam@liamnaylor.com>
2023-05-14 16:41:21 -0700

    Adjust ul heights

--- src/components/home/HomeCardPanel.js
+++ src/components/home/HomeCardPanel.js
@@ -4,8 +4,8 @@ export default function HomeCardPanel({ children, bgColor, textColor, type }) {
   return (
     <article className={`flex flex-col items-center justify-start w-full bg-${bgColor}`}>
       <h1 className={`text-${textColor} sm:text-h2-mobile md:text-h2-tablet lg:text-h2-desktop mt-10 font-semibold`}>{children}</h1>
-      <div className="w-full">
-        <ul>
+      <div className="flex-1 w-full">
+        <ul className="flex flex-col items-center">
           <li>BC Ferries website</li>
           <li>BC Ferries app</li>
           <li>BC Ferries app</li>
