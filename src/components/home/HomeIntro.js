6bcd6ae27b366dd34c89b988a520572417a6ab3b
Liam Naylor <liam@liamnaylor.com>
2023-05-14 17:13:11 -0700

    Add ella photo + img element

--- src/components/home/HomeIntro.js
+++ src/components/home/HomeIntro.js
@@ -1,8 +1,11 @@
 export default function HomeIntro() {
   return (
-    <section className="flex sm:flex-col md:flex-row lg:flex-row justify-center">
-      <p>Intro blurb!</p>
+    <section className="flex sm:flex-col md:flex-row lg:flex-row items-center justify-center">
+      <p>Hi, I'm Liam.</p>
       <img
+        src={'https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/liamnaylor_bw.jpg'}
+        height={200}
+        width={200}
       />
     </section>
   );
