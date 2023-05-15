53016a275297f2b9a4e0a259054e558ceb4b2816
Liam Naylor <liam@liamnaylor.com>
2023-05-15 13:11:41 -0700

    Adjust height of image cards + add responsive font sizes

--- src/components/home/HomeCardProjectItem.js
+++ src/components/home/HomeCardProjectItem.js
@@ -2,18 +2,15 @@ export default function HomeCardPanelItem({ project }) {
   return (
     <li className="w-full relative">
       <div className="absolute top-1/2 left-0 right-0 transform -translate-y-1/2 text-h1-mobile text-white text-center bg-opacity-50 p-2 z-10">
-        <h2 className="inline-block">{project.name}</h2>
+        <h2 className="inline-block sm:text-h2-mobile md:text-h2-tablet lg:text-h2-desktop">{project.name}</h2>
       </div>
-      <div className="relative">
+      <div class="w-full h-48 overflow-hidden">
         <img
           src={project.image}
           alt={project.name}
-          className="object-cover transition-all duration-300"
-          style={{
-            clipPath: 'inset(-5% -5% -5% -5%)',
-          }}
+          className="object-cover"
         />
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-white opacity-25"></div>
+        <div className="absolute top-0 left-0 right-0 bottom-0 bg-white opacity-30"></div>
       </div>
     </li>
   );
