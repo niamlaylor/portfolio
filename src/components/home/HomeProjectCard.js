b49a22ac0cb6f3e44d3baccc0420f626f8e0c1a2
Liam Naylor <liam@liamnaylor.com>
2023-05-17 13:58:12 -0700

    Add project card components

---
+++ src/components/home/HomeProjectCard.js
@@ -0,0 +1,13 @@
+export default function HomeProjectCard({ project }) {
+  return (
+    <div className="w-full h-48 flex-1 overflow-hidden">
+      <img src={project.image} alt={project.name} className="object-cover w-full h-full" />
+      <div className="absolute top-0 left-0 right-0 bottom-0 bg-white opacity-20"></div>
+      <div className="absolute top-1/2 left-0 right-0 transform -translate-y-1/2 text-h1-mobile text-white text-center p-2 z-10">
+        <h2 className="inline-block sm:text-h3-mobile md:text-h3-tablet font-semibold">
+          {project.name}
+        </h2>
+      </div>
+    </div>
+  );
+};
