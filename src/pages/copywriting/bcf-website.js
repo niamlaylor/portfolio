37cf296e5f5b10f58de207a6bff6d9c0ff6e1027
Liam Naylor <liam@liamnaylor.com>
2023-05-18 10:50:11 -0700

    Update content + syntax

--- src/pages/copywriting/bcf-website.js
+++ src/pages/copywriting/bcf-website.js
@@ -26,7 +26,7 @@ export default function CopywritingInfo() {
                   <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path fill-rule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clip-rule="evenodd" />
                   </svg>
-                  <span><strong class="font-semibold text-gray-900">Content design:</strong> Led a complete revamp of our corporate voice and tone and condensed more than 4300 pages on our old website down to ~400 concise and SEO-friendly pages.</span>
+                  <span><strong class="font-semibold text-gray-900">Content design & SEO:</strong> Led a complete revamp of our corporate voice and tone and condensed more than 4300 pages on our old website down to ~400 concise and SEO-friendly pages.</span>
                 </li>
                 <li class="flex gap-x-3">
                   <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
