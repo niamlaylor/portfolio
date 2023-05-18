681afaff56293eb34d1ec6d75e9c51040a835b5c
Liam Naylor <liam@liamnaylor.com>
2023-05-18 10:42:25 -0700

    Populate bcf website content

--- src/pages/copywriting/bcf-website.js
+++ src/pages/copywriting/bcf-website.js
@@ -7,38 +7,39 @@ export default function CopywritingInfo() {
         <div class="lg:col-span-2 lg:col-start-1 lg:row-start-1 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div class="lg:pr-4">
             <div class="lg:max-w-lg">
-              <p class="text-base font-semibold leading-7 text-indigo-600">BC Ferries</p>
+              <p class="text-base font-semibold leading-7 text-blue-500">BC Ferries</p>
               <h1 class="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">Website content design and UX writing</h1>
-              <p class="mt-6 text-xl leading-8 text-gray-700">The BC Ferries website allows millions of British Columbians and tourists to book, plan and manage their ferry travel.</p>
+              <p class="mt-6 text-xl leading-8 text-gray-700">Originally launched in 2006, the BC Ferries website is a critical tool for millions of British Columbians and tourists to book, plan and manage their ferry travel.</p>
             </div>
           </div>
         </div>
         <div class="-ml-12 -mt-12 p-12 lg:sticky lg:top-4 lg:col-start-2 lg:row-span-2 lg:row-start-1 lg:overflow-hidden">
-          <img class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[50rem]" src="https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/bc-ferries-inside-passage-WEB.jpg" alt="" />
+          <img class="w-[48rem] max-w-none rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10 sm:w-[50rem]" src="https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/bc-ferries-inside-passage-center-WEB.jpg" alt="" />
         </div>
         <div class="lg:col-span-2 lg:col-start-1 lg:row-start-2 lg:mx-auto lg:grid lg:w-full lg:max-w-7xl lg:grid-cols-2 lg:gap-x-8 lg:px-8">
           <div class="lg:pr-4">
             <div class="max-w-xl text-base leading-7 text-gray-700 lg:max-w-lg">
-              <p>Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id. Id dolor praesent donec est. Odio penatibus risus viverra tellus varius sit neque erat velit. Faucibus commodo massa rhoncus, volutpat. Dignissim sed eget risus enim. Mattis mauris semper sed amet vitae sed turpis id.</p>
-              <ul role="list" class="mt-8 space-y-8 text-gray-600">
+              <p class="mb-2">Our team led a complete redesign of the BC Ferries website from the ground up, including a reimagined booking experience, better organized web content and schedules, new current conditions to check the status of sailings, and more.</p><p>The original website had an out-of-date design that was pain to use on a mobile device, so our strategy was to create a crisp and concise layout with a mobile-first, responsive layout.</p>
+              <h2 class="mt-8"><strong class="font-semibold text-gray-900">My role:</strong></h2>
+              <ul role="list" class="mt-4 space-y-8 text-gray-600">
                 <li class="flex gap-x-3">
                   <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path fill-rule="evenodd" d="M5.5 17a4.5 4.5 0 01-1.44-8.765 4.5 4.5 0 018.302-3.046 3.5 3.5 0 014.504 4.272A4 4 0 0115 17H5.5zm3.75-2.75a.75.75 0 001.5 0V9.66l1.95 2.1a.75.75 0 101.1-1.02l-3.25-3.5a.75.75 0 00-1.1 0l-3.25 3.5a.75.75 0 101.1 1.02l1.95-2.1v4.59z" clip-rule="evenodd" />
                   </svg>
-                  <span><strong class="font-semibold text-gray-900">My role:</strong> Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.</span>
+                  <span><strong class="font-semibold text-gray-900">Content design:</strong> Led a complete revamp of our corporate voice and tone and condensed more than 4300 pages on our old website down to ~400 concise and SEO-friendly pages.</span>
                 </li>
                 <li class="flex gap-x-3">
                   <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path fill-rule="evenodd" d="M10 1a4.5 4.5 0 00-4.5 4.5V9H5a2 2 0 00-2 2v6a2 2 0 002 2h10a2 2 0 002-2v-6a2 2 0 00-2-2h-.5V5.5A4.5 4.5 0 0010 1zm3 8V5.5a3 3 0 10-6 0V9h6z" clip-rule="evenodd" />
                   </svg>
-                  <span><strong class="font-semibold text-gray-900">Challenges:</strong> Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</span>
+                  <span><strong class="font-semibold text-gray-900">UX writing:</strong> Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.</span>
                 </li>
                 <li class="flex gap-x-3">
                   <svg class="mt-1 h-5 w-5 flex-none text-indigo-600" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
                     <path d="M4.632 3.533A2 2 0 016.577 2h6.846a2 2 0 011.945 1.533l1.976 8.234A3.489 3.489 0 0016 11.5H4c-.476 0-.93.095-1.344.267l1.976-8.234z" />
                     <path fill-rule="evenodd" d="M4 13a2 2 0 100 4h12a2 2 0 100-4H4zm11.24 2a.75.75 0 01.75-.75H16a.75.75 0 01.75.75v.01a.75.75 0 01-.75.75h-.01a.75.75 0 01-.75-.75V15zm-2.25-.75a.75.75 0 00-.75.75v.01c0 .414.336.75.75.75H13a.75.75 0 00.75-.75V15a.75.75 0 00-.75-.75h-.01z" clip-rule="evenodd" />
                   </svg>
-                  <span><strong class="font-semibold text-gray-900">Overcoming challenges:</strong> Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</span>
+                  <span><strong class="font-semibold text-gray-900">UI/UX design:</strong> Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.</span>
                 </li>
               </ul>
               <p class="mt-8">Et vitae blandit facilisi magna lacus commodo. Vitae sapien duis odio id et. Id blandit molestie auctor fermentum dignissim. Lacus diam tincidunt ac cursus in vel. Mauris varius vulputate et ultrices hac adipiscing egestas. Iaculis convallis ac tempor et ut. Ac lorem vel integer orci.</p>
