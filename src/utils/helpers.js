1ec5b06a7f5b9026bcddf03086222afe303cb194
Liam Naylor <liam@liamnaylor.com>
2023-05-25 16:45:25 -0600

    Delete rss feed packages

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,11 +1,3 @@
-import FeedParser from "feedparser";
-
 export async function fetchRssFeed() {
-  const feedParser = new FeedParser();
-  const feed = await new Promise((resolve, reject) => {
-    feedParser.on('error', reject);
-    feedParser.on('end', resolve);
-    feedParser.write(yourRssXmlData);
-  });
-  return feed.items;
+
 };
