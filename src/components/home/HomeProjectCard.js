f383001a5938daa2f6945f08f501a7e70794e169
Liam Naylor <liam@liamnaylor.com>
2023-05-29 07:46:15 -0700

    Remove card img + change to white background

--- src/components/home/HomeProjectCard.js
+++ src/components/home/HomeProjectCard.js
@@ -1,10 +1,8 @@
 export default function HomeProjectCard({ project }) {
   return (
     <a href="/copywriting/bcf-website">
-      <div className="w-full h-48 flex-1 overflow-hidden">
-        <img src={project.image} alt={project.name} className="object-cover w-full h-full" />
-        <div className="absolute top-0 left-0 right-0 bottom-0 bg-white opacity-20"></div>
-        <div className="absolute top-1/2 left-0 right-0 transform -translate-y-1/2 text-h1-mobile text-white text-center p-2 z-10">
+      <div className="w-full h-48 flex-1 overflow-hidden bg-white">
+        <div className="absolute top-1/2 left-0 right-0 transform -translate-y-1/2 text-h1-mobile text-center p-2 z-10">
             <h2 className="inline-block sm:text-h3-mobile md:text-h3-tablet font-semibold">
               {project.name}
             </h2>
