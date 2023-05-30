27923b7c468fb2550f63e6412b80f7cc91e8d474
Liam Naylor <liam@liamnaylor.com>
2023-05-30 07:51:38 -0700

    Add html/css logos

--- src/components/home/languages/Languages.js
+++ src/components/home/languages/Languages.js
@@ -1,16 +1,38 @@
 export default function Languages() {
   return (
     <div className="flex flex-row mt-7">
-      <img
-      src="https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/js-gs.jpg"
-      loading="lazy"
-      className="rounded-tl-lg h-10 mr-2"
-      />
-      <img
-      src="https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/rb-gs.jpg"
-      loading="lazy"
-      className="h-10 ml-2"
-      />
+      <div className="flex flex-col items-center text-center">
+        <img
+          src="https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/js-gs.jpg"
+          loading="lazy"
+          className="rounded-tl-lg h-10"
+        />
+        <span className="text-xs mt-1">JavaScript</span>
+      </div>
+      <div className="flex flex-col items-center text-center ml-3">
+        <img
+          src="https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/rb-gs.jpg"
+          loading="lazy"
+          className="h-10"
+        />
+        <span className="text-xs mt-1">Ruby</span>
+      </div>
+      <div className="flex flex-col items-center text-center ml-3">
+        <img
+          src="https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/rb-gs.jpg"
+          loading="lazy"
+          className="h-10"
+        />
+        <span className="text-xs mt-1">Ruby</span>
+      </div>
+      <div className="flex flex-col items-center text-center ml-3">
+        <img
+          src="https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/rb-gs.jpg"
+          loading="lazy"
+          className="h-10"
+        />
+        <span className="text-xs mt-1">Ruby</span>
+      </div>
     </div>
-  )
+  );
 }
