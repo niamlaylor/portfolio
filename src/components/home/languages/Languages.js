939d263d37cef35bc206ee5319f1df6d9cd5da09
Liam Naylor <liam@liamnaylor.com>
2023-05-30 07:55:09 -0700

    Add border to laguage logos

--- src/components/home/languages/Languages.js
+++ src/components/home/languages/Languages.js
@@ -1,6 +1,6 @@
 export default function Languages() {
   return (
-    <div className="flex flex-row mt-7">
+    <div className="flex flex-row mt-7 border border-gray-300 rounded-lg p-6">
       <div className="flex flex-col items-center text-center">
         <img
           src="https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/js-gs.jpg"
@@ -19,19 +19,19 @@ export default function Languages() {
       </div>
       <div className="flex flex-col items-center text-center ml-3">
         <img
-          src="https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/rb-gs.jpg"
+          src="https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/html-gs.jpg"
           loading="lazy"
           className="h-10"
         />
-        <span className="text-xs mt-1">Ruby</span>
+        <span className="text-xs mt-1">HTML</span>
       </div>
       <div className="flex flex-col items-center text-center ml-3">
         <img
-          src="https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/rb-gs.jpg"
+          src="https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/css-gs.jpg"
           loading="lazy"
           className="h-10"
         />
-        <span className="text-xs mt-1">Ruby</span>
+        <span className="text-xs mt-1">CSS</span>
       </div>
     </div>
   );
