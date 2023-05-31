670258dd147e534472d1897cfa6478a42c0e9c5a
Liam Naylor <liam@liamnaylor.com>
2023-05-31 15:14:37 -0700

    Add hover blue for experience

--- src/components/home/languages/Experience.js
+++ src/components/home/languages/Experience.js
@@ -4,13 +4,13 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 export default function Experience() {
   return (
     <div className="flex flex-row mt-10 w-full items-center justify-center text-center rounded-lg">
-      <div className={`flex flex-col mr-10`}>
-        <FontAwesomeIcon icon="fa-solid fa-code" size="4x" className={"mb-5"}/>
-        <Heading2>1+ year</Heading2>
-      </div>
-      <div className={`flex flex-col ml-10`}>
+      <div className={`flex flex-col mr-10 experience-col`}>
         <FontAwesomeIcon icon="fa-solid fa-pen-to-square" size="4x" className={"mb-5"}/>
         <Heading2>5+ years</Heading2>
+      </div>
+      <div className={`flex flex-col ml-10 experience-col`}>
+        <FontAwesomeIcon icon="fa-solid fa-code" size="4x" className={"mb-5"}/>
+        <Heading2>1+ year</Heading2>
       </div>
     </div>
   );
