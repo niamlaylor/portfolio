b83759076ecd1e06e56fe52efb3b744b1dd2d655
Liam Naylor <liam@liamnaylor.com>
2023-05-31 16:38:45 -0700

    Fix hydration error + refactor icon use

--- src/components/buttons/MainCTA.js
+++ src/components/buttons/MainCTA.js
@@ -1,12 +1,7 @@
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-
 export default function MainCTA({ children, url }) {
   return (
-    <a href={url} target="_blank" rel="noopener noreferrer" className="mt-12 inline-flex items-center justify-center px-5 py-3 w-1/2 border border-transparent text-xs font-medium rounded-md text-black bg-gray-300 hover:bg-white">
-      {children} 
-      <div className="ml-1">
-        <FontAwesomeIcon icon="fa-solid fa-chevron-right" />
-      </div>
+    <a href={url} rel="noopener noreferrer" className="mt-12 inline-flex items-center justify-center px-5 py-3 w-1/2 border border-transparent text-xs font-medium rounded-md text-black bg-gray-300 hover:bg-white">
+      {`${children} >`}
     </a>
   )
 }
--- src/components/home/languages/Experience.js
+++ src/components/home/languages/Experience.js
@@ -1,21 +1,22 @@
 import Heading2 from '@/components/headers/h2';
 import MainCTA from '@/components/buttons/MainCTA';
+import { faCode, faPenToSquare } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 export default function Experience() {
   return (
     <div className="flex sm:flex-col lg:flex-row mt-10 w-full items-center justify-center text-center rounded-lg">
       <div className={`flex flex-col items-center justify-center text-center sm:m-10 bg-gray-100 hover:bg-gray-200 pt-10 pb-10 sm:w-full lg:w-1/4 rounded-lg border border-gray-200`}>
-        <FontAwesomeIcon icon="fa-solid fa-code" size="4x" className={"mb-5"}/>
+        <FontAwesomeIcon icon={faCode} size="4x" className={"mb-5"}/>
         <Heading2>1+ year</Heading2>
         <h3 className="text-base font-light leading-7">Development experience</h3>
         <MainCTA url={"/development"}>Recent projects</MainCTA>
       </div>
       <div className={`flex flex-col items-center justify-center text-center sm:m-10 bg-gray-100 hover:bg-gray-200 pt-10 pb-10 sm:w-full lg:w-1/4 rounded-lg border border-gray-200`}>
-        <FontAwesomeIcon icon="fa-solid fa-pen-to-square" size="4x" className={"mb-5"}/>
+        <FontAwesomeIcon icon={faPenToSquare} size="4x" className={"mb-5"}/>
         <Heading2>5+ years</Heading2>
         <h3 className="text-base font-light leading-7">Copywriting experience</h3>
-        <MainCTA url={"/development"}>Recent writing</MainCTA>
+        <MainCTA url={"/copywriting"}>Recent writing</MainCTA>
       </div>
     </div>
   );
