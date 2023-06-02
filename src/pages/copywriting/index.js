a33fd00a7324be2366df9bee6d9f404ef0473a98
Liam Naylor <liam@liamnaylor.com>
2023-06-02 07:43:37 -0700

    Add top link hover state

--- src/pages/copywriting/index.js
+++ src/pages/copywriting/index.js
@@ -29,7 +29,7 @@ export default function CopywritingLanding() {
         <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
           <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 sm:grid-cols-2 md:flex lg:gap-x-10">
             {links.map((link) => (
-              <a key={link.name} href={link.href}>
+              <a key={link.name} href={link.href} className={"hover:text-blue-500"}>
                 {link.name} <span aria-hidden="true">&rarr;</span>
               </a>
             ))}
