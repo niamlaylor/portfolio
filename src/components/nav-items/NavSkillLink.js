9259c8b517565c40824660350c1222cddba915ef
Liam Naylor <liam@liamnaylor.com>
2023-06-12 09:47:11 -0700

    Fix bug with relative URL in nav

--- src/components/nav-items/NavSkillLink.js
+++ src/components/nav-items/NavSkillLink.js
@@ -2,7 +2,7 @@ export default function NavSkillLink( { children } ) {
   const lowerCaseChild = children.toLowerCase();
   return (
     <a 
-      href={`${lowerCaseChild}`} 
+      href={`/${lowerCaseChild}`} 
       className="text-black bg-custom-gray text-lg px-4 ml-1 mr-1 font-semibold border border-custom-blue rounded-md hover:bg-blue-500"
     >
       {children}
