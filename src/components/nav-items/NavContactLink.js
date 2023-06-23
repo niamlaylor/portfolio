d78274419778e5dd3fabd57b85cb22e52d181d51
Liam Naylor <liam@liamnaylor.com>
2023-06-23 07:34:51 -0700

    Change to <Image> and <Link> components

--- src/components/nav-items/NavContactLink.js
+++ src/components/nav-items/NavContactLink.js
@@ -1,55 +1,57 @@
+import Link from "next/link";
+import Image from "next/image";
+
 export default function NavContactLink( { children } ) {
   const linkClass = "text-black text-sm ml-1 mr-2 font-semibold";
   return (
     <>
-      { children === 'Github' && <>
-        <a 
-          href="https://github.com/niamlaylor" 
-          target="_blank" 
-          className={`${linkClass} flex`}
-        >
-          <img
-          src={'https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/github-mark.png'}
-          width={20}
-          height={20}
-          className={"mr-1"}
-        />
-          {children}
-        </a>
+      { children === 'Github' && 
+      <>
+        <Link href="https://github.com/niamlaylor" target="_blank">
+          <div className="flex flex-row">
+            <Image 
+              src={'https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/github-mark.png'}
+              width={20}
+              height={20}
+            />
+            <p className={`${linkClass} flex`}>
+              {children}
+            </p>
+          </div>
+        </Link>
       </>
       }
       { children === 'LinkedIn' && 
       <>
-        <a 
-          href="https://www.linkedin.com/in/liamnnaylor/" 
-          target="_blank" 
-          className={`${linkClass} flex`}
-        >
-          <img
-          src={'https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/linkedin-web.png'}
-          width={20}
-          height={20}
-          className={"mr-1"}
-        />
-          {children}
-        </a>
+        <Link href="https://www.linkedin.com/in/liamnnaylor" target="_blank">
+          <div className="flex flex-row">
+            <Image 
+              src={'https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/linkedin-web.png'}
+              width={20}
+              height={20}
+            />
+            <p className={`${linkClass} flex`}>
+              {children}
+            </p>
+          </div>
+        </Link>
       </>
       }
       { children === 'Medium' && 
       <>
-        <a 
-          href="https://medium.com/@liamnaylorr" 
-          target="_blank" 
-          className={`${linkClass} flex`}
-        >
-          <img
-          src={'https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/medium-logo.png'}
-          width={20}
-          height={20}
-          className="rounded-full mr-1"
-        />
-          {children}
-        </a>
+        <Link href="https://medium.com/@liamnaylorr" target="_blank">
+          <div className="flex flex-row">
+            <Image 
+              src={'https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/medium-logo.png'}
+              width={20}
+              height={20}
+              className={"rounded-full"}
+            />
+            <p className={`${linkClass} flex`}>
+              {children}
+            </p>
+          </div>
+        </Link>
       </>
       }
     </>
