b0927f3c84d3ef99e07aaf4986b761a813a02bed
Liam Naylor <liam@liamnaylor.com>
2023-06-23 07:38:54 -0700

    Refactor to use <Link>

--- src/components/nav-items/NavSkillLink.js
+++ src/components/nav-items/NavSkillLink.js
@@ -1,11 +1,12 @@
+import Link from "next/link";
+
 export default function NavSkillLink( { children } ) {
   const lowerCaseChild = children.toLowerCase();
   return (
-    <a 
-      href={`/${lowerCaseChild}`} 
-      className="text-black bg-custom-gray text-lg px-4 ml-1 mr-1 font-semibold border border-custom-blue rounded-md hover:bg-blue-500"
-    >
-      {children}
-    </a>
+    <Link href={`/${lowerCaseChild}`}>
+      <p className="text-black bg-custom-gray text-lg px-4 ml-1 mr-1 font-semibold border border-custom-blue rounded-md hover:bg-blue-500">
+        {children}
+      </p>
+    </Link>
   )
 };
