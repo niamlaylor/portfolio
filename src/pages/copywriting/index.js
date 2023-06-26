5ff721813049ef68f27235f2e926ef73903705c3
Liam Naylor <liam@liamnaylor.com>
2023-06-26 08:05:52 -0700

    Change to <Link>

--- src/pages/copywriting/index.js
+++ src/pages/copywriting/index.js
@@ -34,9 +34,9 @@ export default function CopywritingLanding() {
         <div className="mx-auto mt-10 max-w-2xl lg:mx-0 lg:max-w-none">
           <div className="grid grid-cols-1 gap-x-8 gap-y-6 text-base font-semibold leading-7 sm:grid-cols-2 md:flex lg:gap-x-10">
             {links.map((link) => (
-              <a key={link.name} href={link.href} className={"hover:text-blue-500"}>
+              <Link key={link.name} href={link.href} className={"hover:text-blue-500"}>
                 {link.name} <span aria-hidden="true">&rarr;</span>
-              </a>
+              </Link>
             ))}
           </div>
           <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-14 sm:grid-cols-2 lg:grid-cols-4">
