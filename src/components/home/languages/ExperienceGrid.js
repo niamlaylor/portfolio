f94832afd2b6a81074ae0c14b4fd2bd256af7dae
Liam Naylor <liam@liamnaylor.com>
2023-06-29 07:00:25 -0700

    Fix dev experience

--- src/components/home/languages/ExperienceGrid.js
+++ src/components/home/languages/ExperienceGrid.js
@@ -6,7 +6,7 @@ export default function ExperienceGrid() {
       <Link href="/development" className="flex flex-col-reverse hover:cursor-pointer bg-gray-100 hover:bg-gray-200 sm:p-10 lg:p-14">
         <span aria-hidden="true" className="mt-2 text-xs font-semibold">View projects &rarr;</span>
         <dt className="text-base leading-7 text-gray-800">Development experience</dt>
-        <dd className="text-4xl mb-2 font-bold leading-9 tracking-tight text-gray-900">1+ year</dd>
+        <dd className="text-4xl mb-2 font-bold leading-9 tracking-tight text-gray-900">3+ years</dd>
       </Link>
       <Link href="/copywriting" className="flex flex-col-reverse hover:cursor-pointer bg-gray-100 hover:bg-gray-200 sm:p-10 lg:p-14">
         <span aria-hidden="true" className="mt-2 text-xs font-semibold">View projects &rarr;</span>
