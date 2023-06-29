0ff9c983e66f0443c5acc410320a09ca2797c933
Liam Naylor <liam@liamnaylor.com>
2023-06-29 06:50:50 -0700

    Change experience cards to 1-up on mobile

--- src/components/home/languages/ExperienceGrid.js
+++ src/components/home/languages/ExperienceGrid.js
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function ExperienceGrid() {
   return(
-    <dl className="mt-16 mb-12 grid gap-4 sm:mt-14 grid-cols-2">
+    <dl className="mt-16 mb-12 grid gap-4 sm:mt-14 sm:grid-cols-1 md:grid-cols-2">
       <Link href="/development" className="flex flex-col-reverse hover:cursor-pointer bg-gray-100 hover:bg-gray-200 sm:p-10 lg:p-14">
         <span aria-hidden="true" className="mt-2 text-xs font-semibold">View projects &rarr;</span>
         <dt className="text-base leading-7 text-gray-800">Development experience</dt>
