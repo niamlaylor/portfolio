bfcd5377e6c0f31f0e246e231bec69156968e7bb
Liam Naylor <liam@liamnaylor.com>
2023-07-04 07:50:16 -0700

    Add conditional display for rss feed

--- src/components/home/MediumCarousel.js
+++ src/components/home/MediumCarousel.js
@@ -17,18 +17,22 @@ export default function MediumCarousel() {
       });
   }, []);
 
-  return (
-    <div className="mt-12">
-      {rssData.length > 0 && <Heading2>Medium articles</Heading2>}
-      <dl className="grid grid-cols-1 gap-4 sm:mt-14 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
-        {rssData.length > 0 && rssData.map((article, index) => (
-          <div key={index} className="flex flex-col-reverse hover:bg-gray-200 p-5">
-            <ProjectCTA url={article.link[0]}>Read article</ProjectCTA>
-            <dd className="text-2xl font-bold leading-9 tracking-tight text-gray-900">{article.title[0]}</dd>
-            <dd className="text-sm font-bold leading-9 tracking-tight text-blue-500">{article.pubDate[0]}</dd>
-          </div>
-        ))}
-      </dl>
-    </div>
-  );
+  if (rssData.length > 0) {
+    return (
+      <div className="mt-12">
+        <Heading2>Medium articles</Heading2>
+        <dl className="grid grid-cols-1 gap-4 sm:mt-14 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+          {rssData.map((article, index) => (
+            <div key={index} className="flex flex-col-reverse hover:bg-gray-200 p-5">
+              <ProjectCTA url={article.link[0]}>Read article</ProjectCTA>
+              <dd className="text-2xl font-bold leading-9 tracking-tight text-gray-900">{article.title[0]}</dd>
+              <dd className="text-sm font-bold leading-9 tracking-tight text-blue-500">{article.pubDate[0]}</dd>
+            </div>
+          ))}
+        </dl>
+      </div>
+    )
+  } else {
+    return <div></div>
+  }
 }
