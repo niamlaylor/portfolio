44a4421588c2197e04d967cf1778702a40267530
Liam Naylor <liam@liamnaylor.com>
2023-07-04 06:59:55 -0700

    Add error handling for api call fail

--- src/components/home/MediumCarousel.js
+++ src/components/home/MediumCarousel.js
@@ -2,26 +2,32 @@ import Heading2 from '../headers/h2';
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
-
 export default function MediumCarousel() {
   const [rssData, setRssData] = useState([]);
 
   useEffect(() => {
     axios.get('/api/rss')
-    .then((mediumData) => {
-      setRssData(mediumData);
-      console.log('Component log: ', mediumData.data[0].title[0]);
-    })
+      .then((response) => {
+        setRssData(response.data);
+        console.log('Component log: ', response.data[0]);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
   }, []);
-
-  const articleArray = rssData.data.map((article) => {
-    return <p>{article.title[0]}</p>
-  })
 
   return (
     <div>
-      <Heading2>Medium articles</Heading2>
-      {articleArray}
+      {rssData.length > 0 && <Heading2>Medium articles</Heading2>}
+      <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-14 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
+        {rssData.length > 0 && rssData.map((article, index) => (
+          <div key={index} className="flex flex-col-reverse hover:bg-gray-200 p-5">
+            <dt className="text-base leading-7 text-gray-800">{article.title[0]}</dt>
+            <dd className="text-2xl font-bold leading-9 tracking-tight text-gray-900">{article.title[0]}</dd>
+            <dd className="text-sm font-bold leading-9 tracking-tight text-blue-500">{article.pubDate[0]}</dd>
+          </div>
+        ))}
+      </dl>
     </div>
-  )
-};
+  );
+}
--- src/pages/api/rss.js
+++ src/pages/api/rss.js
@@ -2,9 +2,14 @@ import axios from "axios";
 import { parseStringPromise } from 'xml2js';
 
 const fetchData = async (req, res) => {
-  const response = await axios.get("https://medium.com/feed/@liamnaylorr");
-  const data = await parseStringPromise(response.data);
-  res.status(200).json(data.rss.channel[0].item);
+  try {
+    const response = await axios.get("https://medium.com/feed/@liamnaylorr");
+    const data = await parseStringPromise(response.data);
+    res.status(200).json(data.rss.channel[0].item);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(200).json([]);
+  }
 };
 
 export default fetchData;
