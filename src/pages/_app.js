a882a7a2f2fd5e01a27cada41e541a26624d2762
Liam Naylor <liam@liamnaylor.com>
2023-07-11 16:55:12 -0700

    add scripts for GA

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,4 +1,5 @@
 import Layout from '@/components/Layout';
+import Script from 'next/script';
 import '@/styles/globals.css';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
@@ -9,6 +10,20 @@ library.add(fas);
 export default function App({ Component, pageProps }) {
   return (
     <Layout>
+      <Script 
+        strategy={"lazyOnload"} 
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+      />
+      <Script strategy={"lazyOnload"}>
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+          page_path: window.location.pathname,
+          });
+        `}
+      </Script>
       <Component {...pageProps} />
     </Layout>
   );
