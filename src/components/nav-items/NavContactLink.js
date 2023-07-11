4d64708dfb13beab449c062a88ed7a2e5e6f1296
Liam Naylor <liam@liamnaylor.com>
2023-07-11 09:17:15 -0700

    remove redundant imports

--- src/components/nav-items/NavContactLink.js
+++ src/components/nav-items/NavContactLink.js
@@ -1,9 +1,6 @@
-import Link from "next/link";
-import Image from "next/image";
 import NavContactLinkItem from "./NavContactLinkItem";
 
 export default function NavContactLink( { children } ) {
-  const linkClass = "text-black text-sm ml-1 mr-2 font-semibold";
   return (
     <>
       { children === 'Github' && 
