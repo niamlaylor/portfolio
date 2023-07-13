c3377aaef9171364811261c61d1381aa28aa787e
Liam Naylor <liam@liamnaylor.com>
2023-07-12 18:55:15 -0700

    set transparent bg + add border

--- src/components/buttons/LabelPill.js
+++ src/components/buttons/LabelPill.js
@@ -1,11 +1,11 @@
 export default function LabelPill({ children, index }) {
   if (index === 0) {
     return (
-      <button className="py-1 px-2 no-underline rounded-full text-white font-sans font-light text-xs bg-gray-500 btn-primary cursor-auto hover:bg-gray-100 hover:text-black">{children}</button>
+      <button className="py-1 px-2 no-underline rounded-full text-black font-sans font-light text-xs bg-transparent btn-primary border border-gray-500 cursor-auto hover:bg-gray-100">{children}</button>
     )
   } else {
     return (
-      <button className="py-1 px-2 no-underline rounded-full text-white font-sans font-light text-xs bg-gray-500 btn-primary cursor-auto hover:bg-gray-100 hover:text-black ml-2">{children}</button>
+      <button className="py-1 px-2 no-underline rounded-full text-black font-sans font-light text-xs bg-transparent btn-primary border border-gray-500 cursor-auto hover:bg-gray-100 ml-2">{children}</button>
     )
   }
 }
