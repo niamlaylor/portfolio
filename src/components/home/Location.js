121be784bcf642b9a35a8305ff32f8ae22a1415e
Liam Naylor <liam@liamnaylor.com>
2023-07-18 15:57:56 -0700

    Change to gray text + add underline hover

--- src/components/home/Location.js
+++ src/components/home/Location.js
@@ -1,11 +1,12 @@
 import { faLocationDot } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import Link from 'next/link';
 
 export default function Location({ children }) {
   return (
-    <span>
+    <span className={"text-gray-800 hover:underline"}>
       <FontAwesomeIcon className={"mr-2"} icon={faLocationDot} />
-      {children}
+      <Link href={"https://www.google.com/maps/place/Canmore,+AB"} target="_blank">{children}</Link>
     </span>
   )
 }
