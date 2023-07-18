2e5085997c16252b6b5459a52b994b6c75be35e4
Liam Naylor <liam@liamnaylor.com>
2023-07-18 16:33:27 -0700

    add hover + remove pill hover state

--- src/components/buttons/LabelPill.js
+++ src/components/buttons/LabelPill.js
@@ -1,11 +1,11 @@
 export default function LabelPill({ children, index }) {
   if (index === 0) {
     return (
-      <button className="py-1 px-2 no-underline rounded-full text-black font-sans font-light text-xs bg-transparent btn-primary border border-gray-500 cursor-auto hover:bg-gray-100">{children}</button>
+      <button className="py-1 px-2 no-underline rounded-full text-black font-sans font-light text-xs bg-transparent btn-primary border border-gray-500 cursor-auto">{children}</button>
     )
   } else {
     return (
-      <button className="py-1 px-2 no-underline rounded-full text-black font-sans font-light text-xs bg-transparent btn-primary border border-gray-500 cursor-auto hover:bg-gray-100 ml-2">{children}</button>
+      <button className="py-1 px-2 no-underline rounded-full text-black font-sans font-light text-xs bg-transparent btn-primary border border-gray-500 cursor-auto ml-2">{children}</button>
     )
   }
 }
