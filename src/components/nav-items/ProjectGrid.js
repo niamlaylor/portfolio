976d9201e4a87af038dc568b37dfae159a2d6ad8
Liam Naylor <liam@liamnaylor.com>
2023-07-18 16:31:44 -0700

    Remove light gray hover

--- src/components/nav-items/ProjectGrid.js
+++ src/components/nav-items/ProjectGrid.js
@@ -9,7 +9,7 @@ export default function ProjectGrid({ type, stats }) {
     return (
       <dl className="mt-16 grid grid-cols-1 gap-8 sm:mt-14 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-4">
         {stats.map((stat) => (
-          <div key={stat.name} className="flex flex-col-reverse hover:bg-gray-200 p-5">
+          <div key={stat.name} className="flex flex-col-reverse p-5">
             <ProjectCTA url={stat.url}>{stat.cta}</ProjectCTA>
             <dt className="text-base leading-7 text-gray-800">{stat.name}</dt>
             <dd className="text-2xl font-bold leading-9 tracking-tight text-gray-900">{stat.value}</dd>
@@ -18,13 +18,13 @@ export default function ProjectGrid({ type, stats }) {
         ))}
       </dl>
     )
-    
+
   // Development project grid
   } else if (type === 'dev') {
     return (
       <dl className="mt-4 grid grid-cols-1 gap-8 sm:mt-14 sm:grid-cols-1 md:grid-cols-2 lg:grid-cols-3">
         {stats.map((stat) => (
-          <div key={stat.name} className="flex flex-col-reverse hover:bg-gray-200 p-5">
+          <div key={stat.name} className="flex flex-col-reverse p-5">
             <RepoCTA url={stat.repo}>View on Github</RepoCTA>
             <div className="justify-left mt-2 select-none flex">
               {stat.category.map((label, index) => {
