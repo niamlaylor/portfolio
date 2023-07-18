ba1a8db8f03705e104f7a0858b6d7ba59dc1d7df
Liam Naylor <liam@liamnaylor.com>
2023-07-18 15:58:52 -0700

    Add bottom margin to location

--- src/components/home/Location.js
+++ src/components/home/Location.js
@@ -4,7 +4,7 @@ import Link from 'next/link';
 
 export default function Location({ children }) {
   return (
-    <span className={"text-gray-800 hover:underline"}>
+    <span className={"mb-2 text-gray-800 hover:underline"}>
       <FontAwesomeIcon className={"mr-2"} icon={faLocationDot} />
       <Link href={"https://www.google.com/maps/place/Canmore,+AB"} target="_blank">{children}</Link>
     </span>
