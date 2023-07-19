4fd02e4a8918b725faf52d823859c50ddc83868a
Liam Naylor <liam@liamnaylor.com>
2023-07-19 08:08:30 -0700

    Add social github links

--- src/components/nav-items/NavContactLink.js
+++ src/components/nav-items/NavContactLink.js
@@ -6,7 +6,7 @@ export default function NavContactLink( { children } ) {
       { children === 'Github' && 
       <>
         <NavContactLinkItem
-          url={"https://drive.google.com/uc?id=1KuZTEPSxxyfmjJOxIk__U0Q35Y2qdYKL"}
+          url={"https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/github-mark.png"}
           href={"https://github.com/niamlaylor"}
         >
           {children}
@@ -16,7 +16,7 @@ export default function NavContactLink( { children } ) {
       { children === 'LinkedIn' && 
       <>
         <NavContactLinkItem
-          url={"https://drive.google.com/uc?id=1WR5oGV-I_oR4JNfFXDsjxux7b89Mven1"}
+          url={"https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/linkedin-web.png"}
           href={"https://www.linkedin.com/in/liamnnaylor/"}
         >
           {children}
@@ -26,7 +26,7 @@ export default function NavContactLink( { children } ) {
       { children === 'Medium' && 
       <>
         <NavContactLinkItem
-          url={"https://drive.google.com/uc?id=1qEOgCiy5C39ymzVWFwBtuaQSO5L6zWgo"}
+          url={"https://raw.githubusercontent.com/niamlaylor/portfolio/main/public/medium-logo.png"}
           href={"https://medium.com/@liamnaylorr"}
         >
           {children}
